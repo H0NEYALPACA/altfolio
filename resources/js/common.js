b1c24fd19b39055985e0949eeaa733eca22b11e2
h0neyalpaca <dalcom0_0@naver.com>
2022-01-02 23:18:58 +0900

    futsal-on update

--- resources/js/common.js
+++ resources/js/common.js
@@ -6,11 +6,3 @@ document.addEventListener('scroll', function() {
         $('header').css('box-shadow','none');
     }
 });
-
-$('.gnb-wrap li a').hover(function(){
-    $(this).css('transition-duration','0.5s');
-    $(this).css('color','#8e9eeb');
-}, function(){
-    $(this).css('transition-duration','0.5s');
-    $(this).css('color','#333');
-});
